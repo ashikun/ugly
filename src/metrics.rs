006c6e460d35c4eea93562c1fcf3e1d39f4f7a93
Matt Windsor <mattwindsor91@gmail.com>
2022-03-20 23:25:14 +0000

    Remove unused metric gunk from zombiesplit import
    Window metrics aren't used here!  ugly doesn't handle window creation.

--- src/metrics.rs
+++ src/metrics.rs
@@ -1,6 +1,11 @@
-//! Font and window metrics.
-use serde::{Deserialize, Serialize};
-
+//! Metrics used in `ugly`.
+//!
+//! Generally, metrics are `i32`s, even when negative values make no sense.
+//! This is primarily to avoid unnecessary casting and potential overflow/underflow corner cases.
+//!
+//! `ugly` itself doesn't specify the units used for lengths; they are backend-dependent.
+//! In SDL2, for instance, they are logical pixel values.  Future text-based backends may instead
+//! specify character cell units.
 pub mod anchor;
 pub mod axis;
 pub mod point;
@@ -12,40 +17,3 @@ pub use axis::Axis;
 pub use point::Point;
 pub use rect::Rect;
 pub use size::{Length, Size};
-
-/// Window metrics.
-#[derive(Copy, Clone, Debug, Deserialize, Serialize)]
-#[serde(default)]
-pub struct Window {
-    /// The window width.
-    pub win_w: Length,
-    /// The window height.
-    pub win_h: Length,
-    /// Standard padding on contents.
-    pub padding: Length,
-}
-
-/// Default window metrics.
-impl Default for Window {
-    fn default() -> Self {
-        Self {
-            win_w: 320,
-            win_h: 640,
-            padding: 1,
-        }
-    }
-}
-
-impl Window {
-    /// Gets the configured size for the window.
-    ///
-    /// This determines the size that the window will take up initially; the
-    /// window may be resized later on.
-    #[must_use]
-    pub fn win_size(&self) -> Size {
-        Size {
-            w: self.win_w,
-            h: self.win_h,
-        }
-    }
-}
