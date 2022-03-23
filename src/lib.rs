0ab25e5cf3d46fb308ac4cb6b19d24acf7342335
Matt Windsor <mattwindsor91@gmail.com>
2022-03-23 21:49:34 +0000

    Add top-level re-exports
    There was a lot of stuttering that has been alleviated now.

--- src/lib.rs
+++ src/lib.rs
@@ -13,4 +13,9 @@ pub mod metrics;
 pub mod render;
 pub mod text;
 
+// Generally, we re-export anything where the name would stutter; these tend to be the most
+// important type in the respective module anyway.
+
 pub use error::{Error, Result};
+pub use font::Font;
+pub use render::Renderer;
