7cbeb4ed8d54ba9c7a72919a35d136580468644d
Matt Windsor <mattwindsor91@gmail.com>
2022-04-09 12:46:12 +0100

    Add microbenchmark for font layout
    This is slowing down ugly-using code to a crawl, so I need to try
    optimise it.

---
+++ benches/font_layout.rs
@@ -0,0 +1,19 @@
+//! Benchmarks the layout of simple text strings.
+use criterion::{criterion_group, criterion_main, Criterion};
+use ugly::metrics::Point;
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let font = ugly::Font::from_dir("assets/fonts/medium");
+    let metrics = font.metrics().expect("couldn't load font metrics");
+    c.bench_function("prop-kerned", |b| {
+        b.iter(|| {
+            metrics.layout_str(
+                Point::default(),
+                "The quick brown fox jumps over the lazy dog",
+            )
+        })
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
