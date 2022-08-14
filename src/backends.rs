802d8ae4fd66a15594b93ce6c8891314c898ed17
Matt Windsor <mattwindsor91@gmail.com>
2022-08-14 12:12:22 +0100

    Tidy up SDL backend
    Now we just enable the SDL backend whenever the SDL dependency is
    available.

--- src/backends.rs
+++ src/backends.rs
@@ -1,6 +1,6 @@
 //! Backends for `ugly`.
 //!
-//! At the time of writing, only one backend exists: SDL2.
+//! At the time of writing, only one backend exists: SDL2, enabled by the `sdl2` feature and residing in the `ugly::backends::sdl` module.
 
-#[cfg(feature = "backend_sdl")]
+#[cfg(feature = "sdl2")]
 pub mod sdl;
