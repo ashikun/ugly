90bdcce773b9b860bf369de5de8e709407a57ee7
Matt Windsor <mattwindsor91@gmail.com>
2024-06-06 22:08:35 +0100

    Fix broken benchmark code

--- benches/text_write.rs
+++ benches/text_write.rs
@@ -1,19 +1,24 @@
 //! Benchmarks the repeated writing of simple text strings.
-use criterion::{criterion_group, criterion_main, Criterion};
 use std::collections::HashMap;
-use ugly::{colour, font, render, resource, text, Renderer};
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+use ugly::{font, render, resource, text};
 
 /// Benchmarks writing the same string several times in a row without changing anything.
 fn write_same(c: &mut Criterion) {
     let font = ugly::Font::from_dir("assets/fonts/medium");
     let metrics = font.metrics().expect("couldn't load font metrics");
+    let mmap: resource::DefaultingHashMap<(), font::Metrics> =
+        resource::DefaultingHashMap::new(HashMap::default(), metrics);
     c.bench_function("write same thing repeatedly", |b| {
         b.iter_batched(
-            || setup_write(metrics.clone()),
+            setup_write,
             |(logger, writer)| {
                 write_repeatedly(
                     logger,
                     writer,
+                    &mmap,
                     &["the quick brown fox jumps over the lazy dog"],
                 )
             },
@@ -26,13 +31,16 @@ fn write_same(c: &mut Criterion) {
 fn write_alternating(c: &mut Criterion) {
     let font = ugly::Font::from_dir("assets/fonts/medium");
     let metrics = font.metrics().expect("couldn't load font metrics");
+    let mmap: resource::DefaultingHashMap<(), font::Metrics> =
+        resource::DefaultingHashMap::new(HashMap::default(), metrics);
     c.bench_function("write alternating strings repeatedly", |b| {
         b.iter_batched(
-            || setup_write(metrics.clone()),
+            setup_write,
             |(logger, writer)| {
                 write_repeatedly(
                     logger,
                     writer,
+                    &mmap,
                     &[
                         "the quick brown fox jumps over the lazy dog",
                         "JACKDAWS LOVE MY GIANT SPHINX OF QUARTZ",
@@ -46,34 +54,22 @@ fn write_alternating(c: &mut Criterion) {
 
 // TODO(@MattWindsor91): test moving the position and font of the string
 
-fn setup_write(
-    metrics: font::Metrics,
-) -> (
-    render::logger::Logger<Font, Fg, Fg>,
-    text::Writer<Font, Fg, Fg>,
-) {
-    let mmap: resource::DefaultingHashMap<(), font::Metrics> =
-        resource::DefaultingHashMap::new(HashMap::default(), metrics);
-
-    let logger = render::logger::Logger::new(mmap);
-    let mut writer = text::Writer::new();
-    writer.set_font(());
-    writer.set_fg(());
+fn setup_write() -> (render::logger::Logger<(), (), ()>, text::Writer<(), ()>) {
+    let logger = render::logger::Logger::default();
+    let writer = text::Writer::new((), ());
     (logger, writer)
 }
 
-type Font = resource::DefaultingHashMap<(), font::Font>;
-type Fg = resource::DefaultingHashMap<(), colour::Definition>;
-
 #[inline]
 fn write_repeatedly(
-    mut logger: render::logger::Logger<Font, Fg, Fg>,
-    mut writer: text::Writer<Font, Fg, Fg>,
+    mut logger: render::logger::Logger<(), (), ()>,
+    mut writer: text::Writer<(), ()>,
+    mmap: &resource::DefaultingHashMap<(), font::Metrics>,
     things_to_write: &[&str],
 ) {
     for i in 0..things_to_write.len() {
         writer.set_string(things_to_write[i % things_to_write.len()]);
-        writer.layout(logger.font_metrics());
+        writer.layout(mmap);
         writer
             .render(&mut logger)
             .expect("should not fail to render");
